c99b446ee3a2e915dc41c0b68e71db2a36523105
topbasoguzhan <topbasoguzhan@gmail.com>
2025-03-03 14:20:46 +0300

    Dark & Light theme added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Open_Sans } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Team Chat Application",
@@ -25,11 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={font.className}
         >
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            storageKey="discord-theme">
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
